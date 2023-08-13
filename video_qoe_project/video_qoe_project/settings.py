2ca202a4bd16283b2994dad318902c6fb20032ef
BidemiYemisi <ymsoyeleke@yahoo.com>
2023-08-13 14:32:18 +0100

    removed all google cloud addition, added more text to main and survey pages, added in missing videos

--- video_qoe_project/video_qoe_project/settings.py
+++ video_qoe_project/video_qoe_project/settings.py
@@ -10,10 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -21,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5q-txbrm9-9g*!ebz^m9!pscvby3g1+y!5_@5tjrdz3lul(@&('
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
