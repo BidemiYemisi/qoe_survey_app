140b6dea58a22fb41f28c9a8e93ee4fad393c504
BidemiYemisi <ymsoyeleke@yahoo.com>
2023-08-13 20:45:15 +0100

    added video files to git

--- video_qoe_project/video_qoe_project/settings.py
+++ video_qoe_project/video_qoe_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5q-txbrm9-9g*!ebz^m9!pscvby3g1+y!5_@5tjrdz3lul(@&('
 
-
+##5q-txbrm9-9g*!ebz^m9!pscvby3g1+y!5_@5tjrdz3lul(@&(w0g
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
